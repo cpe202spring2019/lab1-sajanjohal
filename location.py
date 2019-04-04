d18d98058ce79a71b5f6c0f1fca24f788845e833
sajohal <sajohal@calpoly.edu>
2019-04-04 10:48:47 -0700

    added boiler plate object functions (eq, repr) reformatted location_tests.py

--- location.py
+++ location.py
@@ -1,13 +1,24 @@
+# Sajan Johal
 # CPE 202 Lab 0
 
+
 # represents a location using name, latitude and longitude
+
 class Location:
     def __init__(self, name, lat, lon):
         self.name = name    # string for name of location
         self.lat = lat      # latitude in degrees (-90 to 90)
         self.lon = lon      # longitude in degrees (-180 to 180)
 
+    def __eq__(self, other):
+        return self.name == other.name and self.lat == other.lat and self.lon == other.lon
+
+    def __repr__(self):
+        string = "Location('" + self.name + "', " + str(self.lat) + ", " + str(self.lon) + ")"
+        return string
+
 # ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
+
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
@@ -15,14 +26,14 @@ def main():
     loc3 = Location("SLO", 35.3, -120.7)
     loc4 = loc1
 
-    print("Location 1:",loc1)
-    print("Location 2:",loc2)
-    print("Location 3:",loc3)
-    print("Location 4:",loc4)
+    print("Location 1:", loc1)
+    print("Location 2:", loc2)
+    print("Location 3:", loc3)
+    print("Location 4:", loc4)
 
-    print("\nLocation 1 equals Location 2:",loc1==loc2)
-    print("Location 1 equals Location 3:",loc1==loc3)
-    print("Location 1 equals Location 4:",loc1==loc4)
+    print("\nLocation 1 equals Location 2:", loc1 == loc2)
+    print("Location 1 equals Location 3:", loc1 == loc3)
+    print("Location 1 equals Location 4:", loc1 == loc4)
 
     locations = [loc1, loc2]
     print(loc1 in locations)
@@ -30,5 +41,6 @@ def main():
     print(loc3 in locations)
     print(loc4 in locations)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
