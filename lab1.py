08651d087c806068223a8f6d694d38fcc92f93ae
sajohal <sajohal@calpoly.edu>
2019-04-04 11:11:43 -0700

    added max_list_iter function and corresponding test cases

--- lab1.py
+++ lab1.py
@@ -1,10 +1,23 @@
 # Sajan Johal
+# Lab 1 Part 1
 
 
 def max_list_iter(int_list):  # must use iteration not recursion
-    """finds the max of a list of numbers and returns the value (not the index)
-    If int_list is empty, returns None. If list is None, raises ValueError"""
-    pass
+    if int_list == []:
+        return None
+    elif int_list is None:
+        raise ValueError
+
+    max_val = -10000000
+    for i in range(len(int_list)):
+        if int_list[i] == list:
+            for j in range(len(int_list[i])):
+                if int_list[i][j] > max_val:
+                    max_val = int_list[i][j]
+        else:
+            if int_list[i] > max_val:
+                max_val = int_list[i]
+    return max_val
 
 
 def reverse_rec(int_list):   # must use recursion
