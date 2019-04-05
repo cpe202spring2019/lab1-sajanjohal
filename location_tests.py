cee93cdec4e38beb80ffa4e282dadf4b15f18312
sajohal <sajohal@calpoly.edu>
2019-04-04 17:00:55 -0700

    added location object test cases

--- location_tests.py
+++ location_tests.py
@@ -4,11 +4,32 @@ from location import *
 
 class TestLab1(unittest.TestCase):
 
-    def test_repr(self):
+    def test_repr_1(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
     
-    # Add more tests!
+    def test_repr_2(self):
+        loc = Location('San Francisco', 123.2, 432)
+        self.assertEqual(repr(loc), "Location('San Francisco', 123.2, 432)")
+
+    def test_repr_3(self):
+        loc = Location('Vegas', 444, 2.33)
+        self.assertEqual(repr(loc), "Location('Vegas', 444, 2.33)")
+
+    def test_eq_1(self):
+        loc1 = Location("SLO", 12.1, -14.2)
+        loc2 = Location("SLO", 12.1, -14.2)
+        self.assertTrue(loc1 == loc2)
+
+    def test_eq_2(self):
+        loc1 = Location("SF", 123, 543)
+        loc2 = Location("SF", 124, 543)
+        self.assertFalse(loc1 == loc2)
+
+    def test_eq_3(self):
+        loc1 = Location("Vegas", 43.2, -12.3)
+        loc2 = Location("Vegas", 43.2, 12.3)
+        self.assertFalse(loc1 == loc2)
 
 
 if __name__ == "__main__":
--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 # some simple functions
 
+
 def factorial(n): # assumes n non-negative integer, a number
     if (n == 0) :
         return 1
@@ -9,25 +10,25 @@ def factorial(n): # assumes n non-negative integer, a number
 
 def fib(n):
     """ Assumes n is non-negative integer"""
-    if ((n == 1) or (n == 0)):
+    if (n == 1) or (n == 0):
         return n
     else:
         return fib(n-1) + fib(n-2)
 
 
-def maxlist_rec(tlist):   #returns max of a list of numbers
-    if (len(tlist) == 0):
+def maxlist_rec(tlist):   # returns max of a list of numbers
+    if len(tlist) == 0:
         raise ValueError('empty list')
-    elif (len(tlist) == 1):
+    elif len(tlist) == 1:
         return tlist[0]
     else:
         templist = tlist[1:len(tlist)]
         temp = maxlist_rec(templist)
-        return (max(tlist[0],temp))
+        return max(tlist[0],temp)
 
 
-def reverse_iter(tempstring):
-    newstring = ""
-    for i in range(len(tempstring)-1,-1,-1)  :
-        newstring = newstring + tempstring[i]
-    return newstring
+def reverse_iter(temp_string):
+    new_string = ""
+    for i in range(len(temp_string)-1, -1, -1):
+        new_string = new_string + temp_string[i]
+    return new_string
